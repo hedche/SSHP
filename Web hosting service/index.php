b6ab510249e06f56d646c48da76af2eb769405c1
Monty Rhodes <64991745+hedche@users.noreply.github.com>
2020-08-26 18:00:37 +0100

    Add files via upload

---
+++ Web hosting service/index.php
@@ -0,0 +1,12 @@
+<!-- Author: Monty Rhodes !-->
+<!-- Headings -->
+<h1 style="color: #042659; text-align: center;"><strong>Remote Monitoring and Control</strong> Interface</h1>
+<h2 style="color: #2e6c80; text-align: center;">Choose one of the following options:</h2>
+<!--  Button to link to Temperature Monitoring page -->
+<p style="text-align: center;">&nbsp;<a href="http://smartrhodes.tech/SSHP/TemperatureMonitor.php"><span style="background-color: #46bd85; color: #ffffff; display: inline-block; padding: 3px 10px; border-radius: 5px;">Temperature Monitor</span></a></p>
+<!-- Button to link to Water Level Monitoring page -->
+<p style="text-align: center;">&nbsp;<a href="http://smartrhodes.tech/SSHP/WaterLevelMonitor.php"><span style="background-color: #46bd85; color: #ffffff; display: inline-block; padding: 3px 10px; border-radius: 5px;">Water Level Monitor</span></a></p>
+<!-- Button to link to Power Monitoring page -->
+<p style="text-align: center;">&nbsp;<a href="http://smartrhodes.tech/SSHP/PowerMonitor.php"><span style="background-color: #46bd85; color: #ffffff; display: inline-block; padding: 3px 10px; border-radius: 5px;">Power Monitor</span></a></p>
+<!-- Botton to link to Relay Control page -->
+<p style="text-align: center;">&nbsp;<a href="http://smartrhodes.tech/SSHP/ControlMain.php"><span style="background-color: #9e0606; color: #ffffff; display: inline-block; padding: 3px 10px; border-radius: 5px;">Control Dashboard</span></a></p>
